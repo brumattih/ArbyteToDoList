5a7b53f5edeefb781ce1a831ab82dca92741ccd6
brumattih <hrpreis@gmail.com>
2020-07-01 19:56:05 -0300

    versão offline

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Auth from './src/screens/Auth'
+import TaskList from './src/screens/TaskList'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Auth);
+AppRegistry.registerComponent(appName, () => TaskList);
